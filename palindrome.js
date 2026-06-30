f1d168bc2b949635b0544a0f7b63a5955dac1376
Abdur-Rahim-bin-Bakkar <webdesignrahim4061@gmail.com>
2026-06-30 19:25:44 +0600

    add isPalindrome provlem

---
+++ palindrome.js
@@ -0,0 +1,25 @@
+const isPalindrome = (x)=>{
+    if(x<0){
+        return false
+    }
+    const numbers = `${x}`;
+    const returnNumbers = []
+    for(let num of numbers){
+        returnNumbers.unshift(num)
+    }
+    if(returnNumbers.join('') === numbers){
+        return true;
+    }
+    return false;
+}
+console.log(isPalindrome(1331))
+
+// AI generated:
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+
+    const str = x.toString();
+    const reverse = str.split("").reverse().join("");
+
+    return str === reverse;
+};
