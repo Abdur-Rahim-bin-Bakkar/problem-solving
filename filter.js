d06656c711eb686b1816012472c4ddbd4a31fa7e
Abdur-Rahim-bin-Bakkar <webdesignrahim4061@gmail.com>
2026-07-06 15:45:49 +0600

    add filter

--- filter.js
+++ filter.js
@@ -1,4 +1,17 @@
 const number = []
-var filter = function(arr, fn) {
+const fun = ()=>{
     
+}
+const filter = function(arr, fn) {
+    const newArray = []
+    let index = 0;
+    for(const ar of arr){
+        const result = fn(ar,index)
+        index++
+        if(result){
+            newArray.push(ar)
+        }
+    }
+    return newArray
 };
+filter([121], fun)
