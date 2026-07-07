faa23c737e0029e758de58bea29b02bc5f59123f
Abdur-Rahim-bin-Bakkar <webdesignrahim4061@gmail.com>
2026-07-07 06:24:22 +0600

    add positive and negative

---
+++ positiveOrNegative.js
@@ -0,0 +1,11 @@
+function processData(input) {
+    if(input < 0){
+        console.log('Negative')
+        return
+    }
+    console.log("Positive")
+} 
+
+processData(2)
+processData(0)
+processData(-2)
