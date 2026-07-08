4b057a63d07517ab6fb01d9db4e5f958505ce12f
Abdur-Rahim-bin-Bakkar <webdesignrahim4061@gmail.com>
2026-07-08 15:27:31 +0600

    finaly  complite removeDuplicates

--- removeDuplicatArray.js
+++ removeDuplicatArray.js
@@ -1,32 +1,12 @@
 var removeDuplicates = function (nums) {
-    count = 0
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                nums[j] = '_'
-                // console.log(nums[i])
-
-                // nums[i] = nums[j]
-                // nums[j] = nums[i]
-            }
+    let write = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[write] = nums[i];
+            write++;
         }
     }
-    // console.log(nums)
-    for (let i = 0; i< nums.length; i++) {
-
-        if (nums[i] === '_') {
-
-            console.log(nums[i])
-        }
-
-        // if(num === 0){
-        //     count +=1
-        // }
-        // if(num){
-        //     count +=1
-        // }
-    }
-    // return count
+    return write;
 };
 
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
